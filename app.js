1a23762d108efefafe3f48299a4e9fab83f09c32
Taylor King <kingtb@appstate.edu>
2015-10-27 23:21:17 -0400

    cleanup some spacing

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var commandQueue = [];
 var speaking = false;
 
 bot.on('message', function (msg) {
-  if(msg.message_id <= latestUpdate) {
+  if (msg.message_id <= latestUpdate) {
     return;
   } else {
     latestUpdate = msg.message_id;
@@ -23,45 +23,45 @@ bot.on('message', function (msg) {
   var ignore = false;
   var skipannounce = false;
   settings = JSON.parse(settings);
-  if(settings[msg.chat.title] !== undefined) {  
-    if(settings[msg.chat.title][msg.from.username] !== undefined) {
-      if(settings[msg.chat.title][msg.from.username].ignore !== undefined) {
+  if (settings[msg.chat.title] !== undefined) {  
+    if (settings[msg.chat.title][msg.from.username] !== undefined) {
+      if (settings[msg.chat.title][msg.from.username].ignore !== undefined) {
         ignore = settings[msg.chat.title][msg.from.username].ignore;
       }
-      if(settings[msg.chat.title][msg.from.username].voice !== undefined) {
+      if (settings[msg.chat.title][msg.from.username].voice !== undefined) {
         voice = settings[msg.chat.title][msg.from.username].voice;
       } 
-      if(settings[msg.chat.title][msg.from.username].skipannounce !== undefined) {
+      if (settings[msg.chat.title][msg.from.username].skipannounce !== undefined) {
         skipannounce = settings[msg.chat.title][msg.from.username].skipannounce;
       }
     }
-    if(ignore) {
+    if (ignore) {
       return;
     }
-    if(voice !== undefined) {
+    if (voice !== undefined) {
       command = "say -v " + "\"" + voice +  "\" ";
-      if(!skipannounce) {
+      if (!skipannounce) {
         command += msg.from.first_name + " says ";
       }
       command += "\"" + msg.text + "\"";
     } else {
       command = "say -v "+ "\"" + defaultVoice +"\" " 
-      if(!skipannounce) {
+      if (!skipannounce) {
         command += msg.from.first_name + " says ";
       }
       command += "\"" + msg.text + "\"";
     }
     console.log("command: " + command);
     commandQueue.push(command);
-    if(!speaking) {
-      speaking=true;
+    if (!speaking) {
+      speaking = true;
       speak();
     }
   }
 });
 
 function speak() {
-  if(commandQueue.length === 0) {
+  if (commandQueue.length === 0) {
     speaking = false;
     return;
   }
